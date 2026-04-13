dae334d7a453df9ed3793c9520e6c42f6b93fe70
Yasmine BOIRE <yasmine.boire@gmail.com>
2026-04-13 15:39:45 +0200

    Update exo_01.cpp

--- td5/src/exo1/exo_01.cpp
+++ td5/src/exo1/exo_01.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <string>
 
+//question 1
+size_t folding_string_hash(std::string const& s, size_t max) {
+    size_t hash {0}; 
+    for(size_t i {0}; i < s.size(); ++i) {
+        hash += s[i];
+        hash %= max;
+    }
+
+    return hash;
+}
+
+//question2
+
+size_t folding_string_ordered_hash(std::string const& s, size_t max) {
+    size_t hash {0}; 
+    for(size_t i {0}; i < s.size(); ++i) {
+        hash += s[i] * (i + 1);
+        hash %= max;
+    }
+
+    return hash;
+}
+
+int main () {
+    std::string str {};
+    std::cout << "Entre une string : ";
+    std::getline(std::cin, str); 
+
+    size_t h1 = folding_string_hash(str, 100);
+    size_t h2 = folding_string_ordered_hash(str, 100);
+
+    std::cout << "la somme exo 1 vaut : " << h1 << std::endl;
+    std::cout << "la somme exo 2 vaut : " << h2 << std::endl;
+
+    return 0;
+}
